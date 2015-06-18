ce81f04a202b821d6fbcc2cc5d28b951f449c3bf
Remy Maucourt <remy.maucourt@yahoo.com>
2015-06-18 21:10:13 +0200

    set views and public folders

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 var express = require('express');
+var stylus = require('stylus');
+var ejs = require('ejs');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(stylus.middleware({
+  src: __dirname + '/resources',
+  dest: __dirname + '/public',
+  compile: function (str, path) {
+    return stylus(str).set('filename', path);
+  },
+  debug: true,
+  force: true
+}));
+
+app.use('/static', express.static(__dirname + '/public/static'));
+
+app.set('view engine', 'ejs');
+
+render_page = function(page, response) {
+  response.setHeader('Content-Type', 'text/html')
+  response.render(page)
+}
+
+app.get('/', function(req, res){
+  render_page('home', res);
 });
 
 var server = app.listen(3000, function () {
-
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
-
 });
