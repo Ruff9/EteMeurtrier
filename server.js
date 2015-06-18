29cb9691c9cdb8a2e59e68e39070ae4dfd048063
Remy Maucourt <remy.maucourt@yahoo.com>
2015-06-18 21:22:30 +0200

    stylus fonctionnel

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/', function(req, res){
   render_page('home', res);
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
