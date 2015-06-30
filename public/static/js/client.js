d1b7614fcc3ebbbb45d6e093d2270daccb9ff4db
Remy Maucourt <remy.maucourt@yahoo.com>
2015-06-30 22:28:28 +0200

    ajoute les infos de contact directement dans la home

---
+++ public/static/js/client.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('#bouton_contact').click(function() {
+  	$('#contact').css('display') == 'none' ? $('#contact').show() : $('#contact').hide()
+  });
+
+  $('.wrapper').click(function() {
+  	$('#contact').hide();
+  });
+})
+
