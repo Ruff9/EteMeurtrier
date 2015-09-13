bd54e79dc6b199f94b0980cc166bde7273e4f798
Remy Maucourt <remy.maucourt@yahoo.com>
2015-09-13 18:56:29 +0200

    retravaille la zone contact

--- app/assets/javascripts/client.js
+++ app/assets/javascripts/client.js
@@ -1,12 +1,12 @@
 $(function(){
-  $('#bouton_contact').click(function() {
-  	$('#contact').css('display') == 'none' ? $('#contact').show() : $('#contact').hide()
-  });
+  // $('#bouton_contact').click(function() {
+  // 	$('#contact').css('display') == 'none' ? $('#contact').show() : $('#contact').hide()
+  // });
 
-  $('.wrapper').click(function() {
-  	$('#contact').hide();
-  });
+  // $('.wrapper').click(function() {
+  // 	$('#contact').hide();
+  // });
 
   $('.popup_launcher').magnificPopup({type:'iframe'});
-
+  $('#bouton_contact').magnificPopup({type:'inline'});
 })
