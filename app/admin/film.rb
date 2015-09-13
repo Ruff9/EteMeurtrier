d68376d713e45c3009d57e1449333b010b00be91
Remy Maucourt <remy.maucourt@yahoo.com>
2015-09-13 17:20:43 +0200

    add description to permit params

--- app/admin/film.rb
+++ app/admin/film.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Film do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-	permit_params :titre, :image, :video_url, :realisateur1, :realisateur2, :chef_operateur, :avec1, :avec2, :annee
+	permit_params :titre, :image, :video_url, :description, :realisateur1, :realisateur2, :chef_operateur, :avec1, :avec2, :annee
 
 	form :html => { :enctype => "multipart/form-data" } do |f|
    	f.inputs do
